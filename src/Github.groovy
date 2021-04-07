0d7a6760dbde5c81ef0d38e2d01cbdffd0999abf
BorisRadenkovic <boris.radenkovickg@gmail.com>
2021-04-07 22:48:28 +0200

    -clone method

--- src/Github.groovy
+++ src/Github.groovy
@@ -5,9 +5,10 @@ class Github {
         brench: 'master',
         credentials: 'MyGithub'
     ]
-    def clone(String repoUrl) {
+    def clone(repoUrl) {
 
        echo "${repoUrl}"
+        echo repoUrl
 
         git branch: params.branch, credentialsId: params.credentials, url: 'https://github.com/brki18/devops_web_goat'
 //git branch: params.branch, params.credentials, url: 'https://github.com/brki18/devops_web_goat'
