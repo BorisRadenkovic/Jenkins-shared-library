063e382539212dfd83f149ad053f1651f7bfcd9a
BorisRadenkovic <80686000+BorisRadenkovic@users.noreply.github.com>
2021-04-07 22:04:36 +0200

    Update Github.groovy

--- src/Github.groovy
+++ src/Github.groovy
@@ -1,6 +1,11 @@
 class Github {
 
-    def clone() {
-
+    params = [
+        
+        brench: 'master',
+        credentials: 'MyGithub'
+    
+    def clone(String url) {
+git branch: params.branch, params.credentials, url: 'https://github.com/brki18/devops_web_goat'
     }
 }
