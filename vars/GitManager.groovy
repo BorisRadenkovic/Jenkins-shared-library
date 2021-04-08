f69dfe507ce3fb8326ced833cd3cc0e880e6dbda
BorisRadenkovic <boris.radenkovickg@gmail.com>
2021-04-08 11:09:16 +0200

    -clone method

---
+++ vars/GitManager.groovy
@@ -0,0 +1,14 @@
+class GitManager {
+
+    static void clone(Script scriptRef, String gitURLString, String branchID, String gitUserID)
+    {
+        scriptRef.checkout([
+                $class: 'GitSCM',
+                branches: [[name: branchID]],
+                doGenerateSubmoduleConfigurations: false,
+                extensions: [[$class: 'CleanCheckout']],
+                submoduleCfg: [],
+                userRemoteConfigs: [[credentialsId: gitUserID, url: gitURLString]]
+        ])
+    }
+}
