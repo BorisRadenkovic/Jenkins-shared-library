ce464c142dcb04bcba30834c6ea9aadc91a9a6a4
BorisRadenkovic <boris.radenkovickg@gmail.com>
2021-04-08 11:47:36 +0200

    -clone method

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,6 +1,6 @@
 import common.Github
 
-static def cloneRepo(String url, String brench, String credentials) {
+ def cloneRepo(String url, String brench, String credentials) {
 
     Github github = new Github(this)
 
